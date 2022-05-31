d8d1ac1530efa5667235e5934c87a5ab4282e710
Benji Bizzell <Benjhaamin@users.noreply.github.com>
2022-05-31 19:24:41 -0400

    Update kc_merge_backup.sh
    Fixed bug with Hex and printf. Handles dirty dest folder better for "highest eml" check. Usage output more clear.

--- scripts/kc_merge_backup.sh
+++ scripts/kc_merge_backup.sh
@@ -4,7 +4,8 @@ if (( $# != 2))
 then
 	echo ""
 	echo "Script will check destination directory for the highest HEX eml name, and then"
-	echo "rename all the emls in the source to incrementing upwards."
+	echo "rename and copies all the emls in the source directory to the destination directory" 
+	echo "each email copied has its filename incremented +1 upwards in HEX."
 	echo ""
 	echo "Missing Parameters. Please provide Source and Destination directories."
 	echo "USAGE: $0 source_directory destination_directory"
@@ -26,7 +27,8 @@ newest_eml=
 ##########################
 
 function find_newest_eml {
-	local check="$(ls -A1p $dest_path | grep -v / | tail -1)"
+	# minor tweak to account for odd files in the dirs
+	local check="$(ls -A1p $dest_path | grep -v / | grep -e .eml | tail -1)"
 
 	if [ -z "$check" ]
 	then
@@ -44,28 +46,23 @@ function strip_eml {
 
 function increment_eml {
     local EML=$1
-    local EML2="$(echo 'obase=ibase=16;'$EML'+1' | bc)"
-	local EML3=$(printf "%08d" $EML2)
-	echo $EML3
+    local EML2="$(echo 'ibase=16;'$EML'+1' | bc)"
+	local EML3="$(printf "%08X" $EML2)"
+	echo "$EML3"
 }
 
 function rename_eml {
 	local EML=$1
-	echo $EML
 	local stripped_newest="$(strip_eml $newest_eml)"
-	echo $stripped_newest
 	local incremented="$(increment_eml $stripped_newest)"
-	echo $incremented
 	local incremented_full="${incremented}.eml"
-	echo $incremented_full
 	newest_eml=$incremented_full
 
-    cp $1 $dest_path/$incremented_full
+    cp $EML "$dest_path"/"$incremented_full"
 }
 
 ########
 # Body
 ########
-
 find_newest_eml
 find $source_path -type f -name '*eml' | while read file; do rename_eml "$file"; done
