29cf8170fc790592309f8df11a0a5935e41dd975
Benji Bizzell <Benjhaamin@users.noreply.github.com>
2022-05-31 14:36:45 -0400

    Update kc_merge_backup.sh
    Cleaned up, but requires a bit of TLC to verify fully.

--- scripts/kc_merge_backup.sh
+++ scripts/kc_merge_backup.sh
@@ -1,41 +1,39 @@
 #!/bin/bash
 
+if (( $# != 2))
+then
+	echo ""
+	echo "Script will check destination directory for the highest HEX eml name, and then"
+	echo "rename all the emls in the source to incrementing upwards."
+	echo ""
+	echo "Missing Parameters. Please provide Source and Destination directories."
+	echo "USAGE: $0 source_directory destination_directory"
+	echo ""
+	exit
+fi
+
 #######################################################
 # Variables
 #######################################################
 
-backup_path=$1
-mailstore_path=$2
-
+source_path=$1
+dest_path=$2
 
 newest_eml=
-newest_eml_decimal=
-
-
 
 ##########################
 # Functions
 ##########################
 
-function check_folder_path {
-	echo "check_folder_path"
-}
-
-function match_backup_items {
-	echo "match_backup_items"
-}
-
-function merge_backup_items {
-	echo "merge_backup_items"
-}
-
-
-### Functions that will handle the filenames for Merge collision
-
-# Pass in the current directory to return the file with highest HEX name
-
 function find_newest_eml {
-	ls -A1p $1 | grep -v / | tail -1
+	local check="$(ls -A1p $dest_path | grep -v / | tail -1)"
+
+	if [ -z "$check" ]
+	then
+		newest_eml=00000000.eml
+	else
+		newest_eml=$check
+	fi
 }
 
 # Strips the eml of the ext so that it can be converted and stored to newest_eml variable
@@ -44,67 +42,30 @@ function strip_eml {
 	echo "${EML%%.*}"
 }
 
-# Used to convert the eml filenames into decimal to increment
-function hex_to_decimal {
-	echo "ibase=16; $1" | bc
+function increment_eml {
+    local EML=$1
+    local EML2="$(echo 'obase=ibase=16;'$EML'+1' | bc)"
+	local EML3=$(printf "%08d" $EML2)
+	echo $EML3
 }
-
-# Used to convert the incremented decimal eml name back into Hex for filename assignment
-function decimal_to_hex {
-	echo "ibase=10; obase=16; $1" | bc
-}
-
-
-### Functions for locating the backed-up emls due to collision
-
-# Accepts an eml file with unique identifier due to collision
-# Perform check against the newest_eml variable
-# Use local variable to assign the incremented eml name in decimal
-# Rename the collision eml to new local variable
-
-# Important Usage note: There is not a convenient way of assigning Global Variables from Function in Bash that I can see.
-# When calling rename_eml, we need to be sure that we also perform the assignment of the echo to newest_eml_decimal.
-
-# We could potentially nest the variable, and the function calls within another function to avoid this.
-# I will explore that further.
 
 function rename_eml {
 	local EML=$1
+	echo $EML
 	local stripped_newest="$(strip_eml $newest_eml)"
-	local decimal_newest="$(hex_to_decimal $stripped_eml)"
-	local incremented_name="$(decimal_newest + 1)"
-	local new_hex="$(decimal_to_hex $incremented_name)"
-	local new_final="${new_hex}.eml"
-	# We might have to use a Find -exec rename on this, we will see. Unclear of benefits one way or another?
-	mv $EML $new_final
-	
-	echo "$incremented_name"
+	echo $stripped_newest
+	local incremented="$(increment_eml $stripped_newest)"
+	echo $incremented
+	local incremented_full="${incremented}.eml"
+	echo $incremented_full
+	newest_eml=$incremented_full
+
+    cp $1 $dest_path/$incremented_full
 }
 
-# Function to find and run the rename function via loop. 
+########
+# Body
+########
 
-function find_collision_emls {
-	# Does this work as expected :o 
-	find . -name '*~' | while read file; do rename_eml "$file"; done
-
-}
-
-echo $backup_path
-echo $mailstore_path
-echo $newest_eml
-echo $newest_eml_decimal
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+find_newest_eml
+find $source_path -type f -name '*eml' | while read file; do rename_eml "$file"; done
